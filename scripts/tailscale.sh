dde0eb72b31c75277826137b7b35e07cc1292815
Belax8 <belax_8@hotmail.com>
2024-02-13 20:48:18 -0700

    Updated the tailscale script to update itself

--- scripts/tailscale.sh
+++ scripts/tailscale.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-# tailscale
 if ! command -v tailscale > /dev/null
 then
   curl -fsSL https://tailscale.com/install.sh | sh
   sudo apt-get install tailscale
   sudo tailscale up
 else
-  echo 'tailscale already installed'
+  sudo tailscale update --yes
 fi
